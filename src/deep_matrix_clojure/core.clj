14875ac09d779b964fcb4ff26bcb0946caf7989f
Ezzeri Esa <esa@squareup.com>
2017-05-18 16:18:43 -0700

    added matrix methods and tests

--- src/deep_matrix_clojure/core.clj
+++ src/deep_matrix_clojure/core.clj
@@ -1,7 +1,88 @@
 (ns deep-matrix-clojure.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(import java.lang.Math)
+(import java.util.Random)
+
+
+(defn transpose [A] (apply mapv vector A))
+
+(defn row [A i] (nth A i))
+
+(defn column [A i] (transpose (vector (nth (transpose A) i))))
+
+(defn random
+  [m n]
+  (loop [counter 0
+         A []]
+    (if (= m counter)
+      A
+      (recur (inc counter) 
+             (conj A (vec (repeatedly n #(.nextGaussian (Random.)))))))))
+
+(defn scalar-row [n i scalar] (assoc-in (vec (repeat n 0)) [i] scalar))
+
+(defn diagonal
+  [n scalar]
+  (loop [counter 0
+         A []]
+    (if (= n counter)
+      A
+      (recur (inc counter)
+             (conj A (scalar-row n counter scalar))))))
+
+(defn operate
+  [operator A B]
+  (loop [counter 0
+         C []]
+    (if (= (count A) counter)
+      C
+      (recur (inc counter) 
+             (conj C (mapv operator (nth A counter) (nth B counter)))))))
+
+(defn dot-product [x y] (reduce + (map * x y)))
+
+(defn times
+  [A B]
+  (let [row-mult (fn [C x] (mapv (partial dot-product x) (transpose C)))]
+    (mapv (partial row-mult B) A)))
+
+(defn exp [x] (Math/exp x))
+
+(defn exp-matrix [A] (mapv (partial mapv exp) A))
+
+(defn row-sum [x] (apply + x))
+
+(defn matrix-sum [A] (row-sum (map row-sum A)))
+
+(defn softmax
+  [A]
+  (let [B (exp-matrix A)
+        column-sum (matrix-sum B)]
+    (loop [counter 0
+           C []]
+      (if (= counter (count A))
+        C
+        (recur (inc counter)
+               (conj C (mapv #(/ % column-sum) (nth B counter))))))))
+
+(defn pos-only [x] (if (pos? x) x 0))
+
+(defn relu [A] (mapv (partial mapv pos-only) A))
+
+(defn nextfloat-row [n] (repeatedly n #(.nextFloat (Random.))))
+
+(defn dropout
+  [A probability]
+  (let [m (count A)
+        n (count (transpose A))
+        over-probability (fn [x] (if (< probability x) 1 0))]
+    (loop [counter 0
+           B []]
+      (if (= m counter)
+        B
+        (recur (inc counter)
+               (conj B (mapv * (nth A counter)
+                               (mapv over-probability (nextfloat-row n)))))))))
+
+(defn shape [A] [(count A) (count (transpose A))])
--- test/deep_matrix_clojure/core_test.clj
+++ test/deep_matrix_clojure/core_test.clj
@@ -2,6 +2,56 @@
   (:require [clojure.test :refer :all]
             [deep-matrix-clojure.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest transpose-test
+  (testing "matrix transpose operation"
+    (is (= [[1 1] 
+            [0 1]]
+           (transpose [[1 0]
+                       [1 1]])))))
+
+(deftest diagonal-test
+  (testing "diagonal matrix constructor"
+    (is (= [[1 0] 
+            [0 1]]
+           (diagonal 2 1)))))
+
+(deftest plus-test
+  (testing "matrix plus operation"
+    (is (= [[3 3] 
+            [3 3]]
+           (operate + [[1 1]
+                       [1 1]]
+                      [[2 2]
+                       [2 2]])))))
+
+(deftest minus-test
+  (testing "matrix minus operation"
+    (is (= [[1 1] 
+            [1 1]]
+           (operate - [[3 3]
+                       [3 3]]
+                      [[2 2]
+                       [2 2]])))))
+
+(deftest times-test
+  (testing "matrix minus operation"
+    (is (= [[4 0] 
+            [0 4]]
+           (times [[2 0]
+                   [0 2]]
+                  [[2 0]
+                   [0 2]])))))
+
+(deftest softmax-test
+  (testing "matrix softmax operation"
+    (is (= [[0.25 0.25] 
+            [0.25 0.25]]
+           (softmax [[1 1]
+                     [1 1]])))))
+
+(deftest relu-test
+  (testing "matrix relu operation"
+    (is (= [[1 0] 
+            [0 1]]
+           (relu [[1 -1]
+                  [-1 1]])))))
